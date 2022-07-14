221b2fbe605014d781363e4c39de8c6b4c7f9eb1
wdroz <william.droz.ch@gmail.com>
2022-07-14 14:08:21 +0200

    feat: more realtime implementation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use std::io::{self, Read};
+use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
-    let mut buffer = vec![];
-    io::stdin().read_to_end(&mut buffer)?;
+    let stdin = io::stdin();
     let mut cpt = 0;
-    for _ in std::str::from_utf8(&buffer).unwrap().split("\n") {
-        cpt += 1;
-        let cpt_str = cpt.to_string();
-        print!("{}{}", cpt, "\r".repeat(cpt_str.len()));
+    for line in stdin.lock().lines() {
+        match line {
+            Err(_) => break, // with ^Z
+            Ok(_) => {
+                cpt += 1;
+                let cpt_str = cpt.to_string();
+                print!("{}{}", cpt, "\r".repeat(cpt_str.len()));
+            }
+        }
     }
     println!();
     Ok(())
